c532f66956c914eaa06f1bfc974fb5b749fef24a
Nimar <l.nimar.b@gmail.com>
2013-01-18 01:03:38 +0100

    changed symlinkdot to include xinitrc

--- symlinkdot.sh
+++ symlinkdot.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc ssh"    # list of files/folders to symlink in homedir
+files="bashrc vimrc xinitrc"    # list of files/folders to symlink in homedir
 
 ##########
 
