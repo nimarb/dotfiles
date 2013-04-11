9547047ede8a7a5d8346f722aa9b98b486c953a4
Nimar <l.nimar.b@gmail.com>
2013-04-11 20:45:45 +0200

    fix if file exists instead of dir exist

--- powersavings.sh
+++ powersavings.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-#for correct echo execution RUN WITH "sudo sh -c ./powersave.sh" OR AS ROOT (careful!)!
+#ATTENTION!!: for correct echo execution RUN WITH "sudo sh -c ./powersave.sh" OR AS ROOT (careful!)!
+
 #powersaving config files
 audio="/etc/modprobe.d/audio_power_save.conf"
 nmiwatchdog="/etc/sysctl.d/disable_watchdog.conf"
@@ -11,35 +12,35 @@ usbsuspend="/etc/udev/rules.d/usb_power_save.rules"
 #copying power saving file
 
 #intel hd audio power saving
-if [ ! -d $audio ]; then
+if [ ! -a $audio ]; then
 	sudo touch $audio
-	sudo "echo "options snd_hda_intel power_save=1" > $audio"
+	sudo echo 'options snd_hda_intel power_save=1' > $audio
 	echo "Created Audio power save config"
 fi	
 
 #turning nmi watchdog off: CAUTION disables hadrware error debugging
-if [ ! -d $nmiwatchdog ]; then
+if [ ! -a $nmiwatchdog ]; then
 	sudo touch $nmiwatchdog
-	sudo echo "kernel.nmi_watchdog = 0" > $nmiwatchdog
+	sudo echo 'kernel.nmi_watchdog = 0' > $nmiwatchdog
 	echo "Disabled NMI Watchdog"
 fi
 
 #pci runtime power management
-if [ ! -d $pciruntime ]; then
+if [ ! -a $pciruntime ]; then
 	sudo touch $pciruntime
 	sudo echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{power/control}="auto"' > $pciruntime
 	echo "Enabled PCI Runtime Power Management"
 fi
 
 #delay vm dirty writeback
-if [ ! -d $vmdirywb ]; then
+if [ ! -a $vmdirywb ]; then
 	sudo touch $vmdirtywb
-	sudo echo "vm.dirty_writeback_centisecs = 1500" > $vmdirtywb
+	sudo echo 'vm.dirty_writeback_centisecs = 1500' > $vmdirtywb
 	echo "Increased VM Dirty Writeback delay"
 fi
 
 #enable USB Auto Suspend
-if [ ! -d $usbsuspend ]; then
+if [ ! -a $usbsuspend ]; then
 	sudo touch $usbsuspend
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control" ATTR{power/control}="auto"' > $usbsuspend
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", TEST=="power/autosuspend" ATTR{power/autosuspend}="2"' >> $usbsuspend
