6bf7ca89265423dd0be33cd85b11958257322dec
Nimar <l.nimar.b@gmail.com>
2014-01-20 21:16:12 +0100

    fixed apostrophes

--- soundconf.sh
+++ soundconf.sh
@@ -6,13 +6,13 @@ usbdac=$(runuser -l nb -c 'asoundconf list' | sed -n '2{p;q}')
 intelhda="PCH"
 
 if [[ "attached" == "$1" ]]; then
-	runuser -l nb -c 'asoundconf set-default-card $usbdac'
+	runuser -l nb -c "asoundconf set-default-card $usbdac"
 	#echo "$usbdac ($usbdac) set as default audio card :)!"
 	exit 1
 fi
 
 if [[ "detached" == "$1" ]]; then
-	runuser -l nb -c 'asoundconf set-default-card $intelhda'
+	runuser -l nb -c "asoundconf set-default-card $intelhda"
 	#echo "intelhda ($intelhda) set as default audio card :)!"
 	exit 1
 fi
