6d82e5be88260d7f20f8b9803181f53d52240f86
Nimar <l.nimar.b@gmail.com>
2014-02-09 19:50:05 +0100

    added code to cycle through wallpaper imgages contained in a certain DIR

--- rc.lua
+++ rc.lua
@@ -39,7 +39,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+-- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/nb/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
@@ -73,10 +74,60 @@ local layouts =
 
 -- {{{ Wallpaper
 if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+	for s = 1, screen.count() do
+		gears.wallpaper.fit(beautiful.wallpaper, s)
     end
 end
+-- }}}
+
+-- {{{ Random Wallpapers
+-- { Function definitions
+-- scan directory, and optionally filter outputs
+function scandir(directory, filter)
+	local i, t, popen = 0, {}, io.popen
+	if not filter then
+		filter = function(s) return true end
+	end
+	print(filter)
+	for filename in popen('ls -a "'..directory..'"'):lines() do
+		if filter(filename) then
+			i = i + 1
+			t[i] = filename
+		end
+	end
+	return t
+end
+-- }
+
+-- configuration - edit to your liking
+wp_index = 1
+wp_timeout = 60
+wp_path = "/home/nb/desktopbgs/"
+wp_filter = function(s) return string.match(s,"%.png$") or string.match(s,"%.jpg$") end
+wp_files = scandir(wp_path, wp_filter)
+
+-- setup the timer
+wp_timer = timer { timeout = wp_timeout }
+wp_timer:connect_signal("timeout", function()
+
+	-- set wallpaper to current index for all screens
+	for s = 1, screen.count() do
+		gears.wallpaper.fit(wp_path .. wp_files[wp_index], s)
+	end
+
+	-- stop the timer (we don't need multiple instances running at the same time)
+	wp_timer:stop()
+
+	-- get next random index
+	wp_index = math.random( 1, #wp_files)
+
+	--restart the timer
+	wp_timer.timeout = wp_timeout
+	wp_timer:start()
+end)
+
+-- initial start when rc.lua is first run
+wp_timer:start()
 -- }}}
 
 -- {{{ Tags
