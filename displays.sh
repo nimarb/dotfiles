6adbfb10996ee316e9e85b18b93d81dbb89d492d
Nimar <l.nimar.b@gmail.com>
2014-02-10 02:39:52 +0100

    added script to config currently connected / disconnected displays and sets them up correctly

---
+++ displays.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# script to set displays up correctly
+
+IN="LVDS1"
+EXTDP="DP1"
+EXTVGA="VGA1"
+if (xrandr | grep "$EXTDP" | grep " connected")
+    then
+		if (xrandr | grep "$EXTVGA" | grep " connected")
+		then
+			xrandr --output $IN --off
+			xrandr --output $EXTDP --rate 60 --auto --output $EXTVGA --left-of $EXTDP --rate 60
+		else
+			xrandr --output $EXTVGA --off
+    		xrandr --output $EXTDP --rate 60 --auto --output $IN --off 
+		fi
+	else
+		xrandr --output $EXTDP --off
+		if (xrandr | grep "$EXTVGA" | grep " connected")
+		then
+			xrandr --output $IN --rate 60 --auto --output $EXTVGA --rate 60 --auto --left-of $IN
+		else
+			xrandr --output $EXTVGA --off
+			xrandr --output $IN --rate 60 --auto 
+		fi
+fi
