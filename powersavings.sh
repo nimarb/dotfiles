5b0826382453f78a93c274e9b92f93c5fef960fb
Nimar <l.nimar.b@gmail.com>
2014-04-07 19:46:24 +0200

    added usb devices for which powersaving should be disabled

--- powersavings.sh
+++ powersavings.sh
@@ -1,45 +1,46 @@
 #/bin/bash
 
-#ATTENTION!!: for correct echo execution RUN WITH "sudo sh -c ./powersave.sh" OR AS ROOT (careful!)!
+# ATTENTION!!: for correct echo execution RUN WITH "sudo sh -c ./powersave.sh" OR AS ROOT (careful!)!
 
-#powersaving config files
+# powersaving config files
 audio="/etc/modprobe.d/audio_power_save.conf"
 nmiwatchdog="/etc/sysctl.d/disable_watchdog.conf"
 pciruntime="/etc/udev/rules.d/pci_pm.rules"
 vmdirtywb="/etc/sysctl.d/dirty_writeback.conf"
 usbsuspend="/etc/udev/rules.d/usb_power_save.rules"
+localrules="/etc/udev/rules.d/91-local.rules"
 
-#copying power saving file
+# copying power saving file
 
-#intel hd audio power saving
+# intel hd audio power saving
 if [ ! -a $audio ]; then
 	sudo touch $audio
 	sudo echo 'options snd_hda_intel power_save=1' > $audio
 	echo "Created Audio power save config"
 fi	
 
-#turning nmi watchdog off: CAUTION disables hadrware error debugging
+# turning nmi watchdog off: CAUTION disables hadrware error debugging
 if [ ! -a $nmiwatchdog ]; then
 	sudo touch $nmiwatchdog
 	sudo echo 'kernel.nmi_watchdog = 0' > $nmiwatchdog
 	echo "Disabled NMI Watchdog"
 fi
 
-#pci runtime power management
+# pci runtime power management
 if [ ! -a $pciruntime ]; then
 	sudo touch $pciruntime
 	sudo echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{power/control}="auto"' > $pciruntime
 	echo "Enabled PCI Runtime Power Management"
 fi
 
-#delay vm dirty writeback
+# delay vm dirty writeback
 if [ ! -a $vmdirywb ]; then
 	sudo touch $vmdirtywb
 	sudo echo 'vm.dirty_writeback_centisecs = 1500' > $vmdirtywb
 	echo "Increased VM Dirty Writeback delay"
 fi
 
-#enable USB Auto Suspend
+# enable USB Auto Suspend
 if [ ! -a $usbsuspend ]; then
 	sudo touch $usbsuspend
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control" ATTR{power/control}="auto"' > $usbsuspend
@@ -47,3 +48,12 @@ if [ ! -a $usbsuspend ]; then
 	echo "Enabled USB Auto Suspend with 2sec delay"
 fi
 
+# disable USB Auto Suspend for Logitech Devices, Microsoft Devices and Apple Devices
+
+if [ ! -a $localrules ]; then
+	sudo touch $localrules
+	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="074b", TEST=="power/control", ATTR{power/control}="on"' > $localrules
+	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c051", TEST=="power/control", ATTR{power/control}="on"' >> $localrules
+	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", TEST=="power/control", ATTR{power/control}="on"' >> $localrules
+	echo "Disabled USB Auto Suspend for Logitech, Apple and MS devices"
+fi
