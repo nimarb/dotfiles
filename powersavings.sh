5120d2da6c97f4f3559337a7cf97d6c893aecec0
Nimar <l.nimar.b@gmail.com>
2014-04-08 01:25:32 +0200

    added a bunch of saving tweaks

--- powersavings.sh
+++ powersavings.sh
@@ -5,10 +5,13 @@
 # powersaving config files
 audio="/etc/modprobe.d/audio_power_save.conf"
 nmiwatchdog="/etc/sysctl.d/disable_watchdog.conf"
-pciruntime="/etc/udev/rules.d/pci_pm.rules"
+pciruntime="/etc/udev/rules.d/44-pci_pm.rules"
 vmdirtywb="/etc/sysctl.d/dirty_writeback.conf"
-usbsuspend="/etc/udev/rules.d/usb_power_save.rules"
+usbsuspend="/etc/udev/rules.d/42-usb_power_save.rules"
 localrules="/etc/udev/rules.d/91-local.rules"
+wakeonlan="/etc/udev/rules.d/43-disable_wol.rules"
+wlan="45-wlan_power_save.rules"
+sata="/etc/udev/rules.d/47-sata_power_save.rules"
 
 # copying power saving file
 
@@ -49,11 +52,32 @@ if [ ! -a $usbsuspend ]; then
 fi
 
 # disable USB Auto Suspend for Logitech Devices, Microsoft Devices and Apple Devices
-
 if [ ! -a $localrules ]; then
 	sudo touch $localrules
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="074b", TEST=="power/control", ATTR{power/control}="on"' > $localrules
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c051", TEST=="power/control", ATTR{power/control}="on"' >> $localrules
 	sudo echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", TEST=="power/control", ATTR{power/control}="on"' >> $localrules
 	echo "Disabled USB Auto Suspend for Logitech, Apple and MS devices"
+fi
+
+# sata power management
+if [ ! -a $sata ]; then
+	sudo touch $sata
+	sudo echo 'KERNEL=="AC*", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="/home/nb/dotfiles/satapwrmngmnt.sh offline"' > $sata
+	sudo echo 'KERNEL=="AC*", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="/home/nb/dotfiles/satapwrmngmnt.sh online"' >> $sata
+	echo "Enabled SATA Power Saving"
+fi
+
+# wake on lan disabled / power saving
+if [ ! -a $wakeonlan ]; then
+	sudo touch $wakeonlan
+	sudo echo 'ACTION=="add", SUBSYSTEM=="net", KERNEL=="eth*" RUN+="/usr/bin/ethtool -s %k wol d"' > $wakeonlan
+	echo "Disabled Wake On Lan"
+fi
+
+# enable wlan powersaving mode
+if [ ! -a $wlan ]; then
+	sudo touch $wlan
+	sudo echo 'ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlan*" RUN+="/usr/bin/iw dev %k set power_save on"' > $wlan
+	echo "Enabled WLAN Power Saving mode"
 fi
