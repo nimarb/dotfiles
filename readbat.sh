b6b7348b422cb8f7ef627d0d93bc97045378582f
Nimar <l.nimar.b@gmail.com>
2015-05-15 15:41:08 +0200

    added logging for charge thresholds

--- readbat.sh
+++ readbat.sh
@@ -10,4 +10,6 @@ echo -e "State: $(cat /sys/devices/platform/smapi/BAT0/state)" >> ~/batstats.txt
 echo -e "Temperature $(cat /sys/devices/platform/smapi/BAT0/temperature)" >> ~/batstats.txt
 echo -e "Model: $(cat /sys/devices/platform/smapi/BAT0/model)" >> ~/batstats.txt
 echo -e "Serial: $(cat /sys/devices/platform/smapi/BAT0/serial)" >> ~/batstats.txt
+echo -e "Start_Charge_Thresh: $(cat /sys/devices/platform/smapi/BAT0/stop_charge_tresh)" >> ~/batstats.txt
+echo -e "Stop_Charge_Thresh: $(cat /sys/devices/platform/smapi/BAT0/start_charge_tresh)" >> ~/batstats.txt
 echo -e "Installed: $(cat /sys/devices/platform/smapi/BAT0/installed)" >> ~/batstats.txt
