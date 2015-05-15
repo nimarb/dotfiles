d94961f331260189bfeb45695188e8179b26def6
Nimar <l.nimar.b@gmail.com>
2015-05-15 15:38:51 +0200

    added start charging threshold

--- setbatterytresh.sh
+++ setbatterytresh.sh
@@ -2,3 +2,4 @@
 
 #this sets the thinkpad's battery stop charging treshold to the desired %
 sudo sh -c 'echo 80 > /sys/devices/platform/smapi/BAT0/stop_charge_thresh'
+sudo sh -c 'echo 60 > /sys/devices/platform/smapi/BAT0/start_charge_thresh'
