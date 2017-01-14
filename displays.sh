5168b95afc63ad0df7e6a85aa5ae47a685bbc1d4
Nimar <l.nimar.b@gmail.com>
2017-01-14 09:28:26 +0100

    formatting, improved rule for no connected external screens

--- displays.sh
+++ displays.sh
@@ -7,26 +7,27 @@ EXTVGA="VGA1"
 EXTHDMI="HDMI1"
 if (xrandr | grep "$EXTDP" | grep " connected")
     then
-	    if (xrandr | grep "$EXTVGA" | grep " connected")
+	if (xrandr | grep "$EXTVGA" | grep " connected")
 	    then
-		    xrandr --output $IN --off
-		    xrandr --output $EXTDP --rate 60 --auto --output $EXTVGA --left-of $EXTDP --rate 60
+		xrandr --output $IN --off
+		xrandr --output $EXTDP --rate 60 --auto --output $EXTVGA --left-of $EXTDP --rate 60
 	    else
-		    xrandr --output $EXTVGA --off
-	    xrandr --output $EXTDP --rate 60 --auto --output $IN --off 
-	    fi
-	else
-	    xrandr --output $EXTDP --off
-	    if (xrandr | grep "$EXTVGA" | grep " connected")
+		xrandr --output $EXTVGA --off
+		xrandr --output $EXTDP --rate 60 --auto --output $IN --off 
+	fi
+    else
+	xrandr --output $EXTDP --off
+	if (xrandr | grep "$EXTVGA" | grep " connected")
 	    then
-		    xrandr --output $IN --rate 60 --auto --output $EXTVGA --rate 60 --auto --left-of $IN
+		xrandr --output $IN --rate 60 --auto --output $EXTVGA --rate 60 --auto --left-of $IN
 	    else
-		    xrandr --output $EXTVGA --off
-		    xrandr --output $IN --rate 60 --auto 
-	    fi
+		xrandr --output $EXTVGA --off
+		xrandr --output $EXTHDMI --off
+		xrandr --output $IN --rate 60 --auto 
+	fi
 fi
 if (xrandr | grep "$EXTHDMI" | grep " connected")
-then
-    xrandr --output $EXTHDMI --rate 60 --auto
-    xrandr --output $IN --off
+    then
+	xrandr --output $EXTHDMI --rate 60 --auto
+	xrandr --output $IN --off
 fi
